3432c18448bca716a9814607401443a49b02ba27
felix <ruiz.felixruiz@gmail.com>
2025-09-01 10:20:49 +0200

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,15 @@ if [ -z "$UNWRAP_HOST" ]; then
     exit 1
 fi
 
+if [ "$UNWRAP_HOST_TLS_ENABLED" = "false" ]; then
+    PROTOCOL="ws"
+    echo "TLS is disabled, using protocol: $PROTOCOL"
+else
+    PROTOCOL="wss"
+    echo "TLS is enabled, using protocol: $PROTOCOL"
+fi
+
 echo "Starting socat"
 socat TCP-LISTEN:22,fork TCP:127.0.0.1:2222 &
 echo "Starting wstunnel"
-exec wstunnel client --log-lvl DEBUG -L tcp://2222:$TARGET_HOST wss://$UNWRAP_HOST
+exec wstunnel client --log-lvl DEBUG -L tcp://2222:$TARGET_HOST $PROTOCOL://$UNWRAP_HOST
